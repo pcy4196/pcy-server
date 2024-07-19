0dcda41c41a6329d1919e546e0f937b4eba4e75b
Park, Chan-Yu <pcy4196@kakao.com>
2024-07-19 22:36:20 +0900

    first commit

---
+++ src/main/java/com/example/pcy_server/PcyServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pcy_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pcy_server/PcyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pcy_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
